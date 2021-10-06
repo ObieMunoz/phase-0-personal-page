56b2b92ace3b3f29f844488b3f74ad88ee7148a3
Obie Munoz <obiemunozjr@gmail.com>
2021-10-06 17:44:50 -0400

    Moved page navbar to script.js. Created projects.html

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
+// CREATE NAVIGATION BARS FOR ALL OF OUR PAGES
+const newNavbar = document.createElement("nav");
+const githLink = document.createElement("a");
+const twitLink = document.createElement("a");
+const linkLink = document.createElement("a");
+const homeLink = document.createElement("a");
+const projectLink = document.createElement("a");
+newNavbar.className = "navbar";
+githLink.href = "https://github.com/obiemunoz";
+githLink.innerText = "Github";
+twitLink.href = "https://www.twitter.com/obiemunoz";
+twitLink.innerText = "Twitter";
+linkLink.href = "https://www.linkedin.com/in/obedmunozjr";
+linkLink.innerText = "LinkedIn";
+homeLink.href = "./index.html";
+homeLink.innerText = "Home";
+projectLink.href = "./projects.html";
+projectLink.innerText = "Projects";
+
+if (document.title === "Obie Munoz | Home") {
+    newNavbar.append(projectLink)
+} else {
+    newNavbar.append(homeLink)
+}
+
+newNavbar.append(githLink)
+newNavbar.append(twitLink)
+newNavbar.append(linkLink)
+document.body.querySelector(".container").prepend(newNavbar)
+
+// CREATE A FOOTER FOR OUR PAGES
 const thisYear = new Date().getFullYear();
 const footer = document.createElement("div");
 const footerLink = document.createElement("a")
-
 footer.className = "footer"
-
 footerLink.href = "mailto:obiemunozjr@gmail.com"
 footerLink.innerText = `Copyright © ${thisYear} Obie Munoz`
-
 footer.append(footerLink)
-
-document.body.append(footer)
+document.body.querySelector(".container").append(footer)
